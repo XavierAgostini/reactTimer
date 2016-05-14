11ec61f980fbc414b4c04e0a3706b6dd8b32ba24
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2016-05-14 16:38:23 -0400

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Nav = require('Nav');
+var Main = (props) => {
+	return (
+		<div>	
+			<div>
+			<Nav/>
+			<p>Main.jsx Rendered</p>
+				<div className="columns medium-6 large-4 small-centered">
+					{props.children}
+				</div>
+			</div>
+		</div>
+	);
+}
+module.exports = Main;
